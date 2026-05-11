cdc05b684b2285a0a1354e71f4b554bc57560288
efzer1337-VP <147754474+efzer1337-VP@users.noreply.github.com>
2026-05-12 00:59:52 +0300

    Cleanup: remove unnecessary assets and files

--- test_encoding.py
+++
@@ -1,6 +0,0 @@
-import sys
-import codecs
-if sys.platform == 'win32' and sys.stdout is not None:
-    sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'replace')
-    sys.stderr = codecs.getwriter('utf-8')(sys.stderr.buffer, 'replace')
-print('「')
